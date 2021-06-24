234f8ddf2327eaccb006110bdbab93d894e530f7
Osvaldo Santana Neto <osantana@osantana.me>
2021-06-24 11:02:47 -0300

    Add Playlists <-> Tracks relationship

--- playlists/models.py
+++ playlists/models.py
@@ -1,16 +1,33 @@
 class Track:
-    def __init__(self, name, artist, album, service='applemusic'):
+    def __init__(self, name, artist, album, playlists=None, service='applemusic'):
         self.name = name
         self.artist = artist
         self.album = album
         self.service = service
 
+        if playlists is None:
+            playlists = []
+        self.playlists = playlists
+
+    def set_playlist(self, playlist):
+        if playlist not in self.playlists:
+            self.playlists.append(playlist)
+
+    def send(self, client):
+        return client.add_track(self)
+
 
 class Playlist:
-    def __init__(self, name, description):
+    def __init__(self, name, description, tracks=None):
         self.name = name
         self.description = description
         self.tracks = []
+
+        if tracks is None:
+            return
+
+        for track in tracks:
+            self.add_track(track)
 
     def __len__(self):
         return len(self.tracks)
@@ -19,4 +36,5 @@ class Playlist:
         return self.tracks[item]
 
     def add_track(self, track):
+        track.set_playlist(self)
         self.tracks.append(track)
--- tests/test_models.py
+++ tests/test_models.py
@@ -3,7 +3,15 @@ import pytest
 from playlists.models import Playlist, Track
 
 
-def test_basic_playlist():
+def test_basic_track():
+    track = Track('Song', 'Author', 'Album')
+    assert track.name == 'Song'
+    assert track.artist == 'Author'
+    assert track.album == 'Album'
+    assert track.playlists == []
+
+
+def test_empty_playlist():
     playlist = Playlist('Name', 'Description')
     assert playlist.name == 'Name'
     assert playlist.description == 'Description'
@@ -12,8 +20,19 @@ def test_basic_playlist():
         assert playlist[0] == 'index-error'
 
 
-def test_basic_track():
+def test_filled_playlist():
     track = Track('Song', 'Author', 'Album')
-    assert track.name == 'Song'
-    assert track.artist == 'Author'
-    assert track.album == 'Album'
+    playlist = Playlist('Name', 'Description', tracks=[track])
+
+    assert track in playlist.tracks
+    assert playlist in track.playlists
+
+
+def test_add_track_to_playlist():
+    track = Track('Song', 'Author', 'Album')
+    playlist = Playlist('Name', 'Description')
+
+    playlist.add_track(track)
+
+    assert track in playlist.tracks
+    assert playlist in track.playlists
